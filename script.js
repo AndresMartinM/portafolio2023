668f572428fa5ee7404da208b9a2433f134b8bfb
Andrés Martin Mena <128084150+AndresMartinM@users.noreply.github.com>
2023-11-04 01:11:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 async function articulo() {
 	
-	var colores = [ "rojo", "azul", "celeste", "verde"];
-	
 	var n = new URLSearchParams(window.location.search).get("nro");
 	n = Number(n);
 	const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolio2023/main/works.json");
@@ -14,19 +12,19 @@ async function articulo() {
 		document.querySelector("#fotos").innerHTML += '<img src="' + x + '" width="600px" class="w-100" alt="imagen del proyecto">'
     });
 	
-	var color = getRandomInt(colores.length);
+	var color = "color";
 	
 	if (n == 1){
-		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + colores[color] + '" href="https://github.com/AndresMartinM" target="_blank" aria-label="Enlace a GitHub">GitHub</a></div>'
+		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + color + '" href="https://github.com/AndresMartinM" target="_blank" aria-label="Enlace a GitHub">GitHub</a></div>'
 	}
 	if (n == 3){
-		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + colores[color] + '" href="https://www.youtube.com/watch?v=4RZLKAtOIvI" target="_blank" aria-label="Enlace a Video">Video</a></div>'
+		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + color + '" href="https://www.youtube.com/watch?v=4RZLKAtOIvI" target="_blank" aria-label="Enlace a Video">Video</a></div>'
 	}
 	if (n == 4){
-		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + colores[color] + '" href="https://andresmartin.itch.io/" target="_blank" aria-label="Enlace a itch.io">itch.io</a></div>'
+		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + color + '" href="https://andresmartin.itch.io/" target="_blank" aria-label="Enlace a itch.io">itch.io</a></div>'
 	}
 	if (n == 6){
-		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + colores[color] + '" href="https://sketchfab.com/andergenio" target="_blank" aria-label="Enlace a Sketchfab">Sketchfab</a></div>'
+		document.querySelector("#texto").innerHTML += '<div class="enlaces col-4"><a class="' + color + '" href="https://sketchfab.com/andergenio" target="_blank" aria-label="Enlace a Sketchfab">Sketchfab</a></div>'
 	}
 }
 
@@ -35,7 +33,7 @@ function getRandomInt(max) {
 }
 
 async function tarjetasm() {
-    const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolioAnim2023/main/works.json");
+    const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolio2023/main/works.json");
     const data = await consulta.json();
     const donde = document.querySelectorAll(".tarjeta-sm");
     donde.forEach(function (t, i) {
@@ -43,15 +41,15 @@ async function tarjetasm() {
     });
 }
 async function tarjetamd() {
-    const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolioAnim2023/main/works.json");
+    const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolio2023/main/works.json");
     const data = await consulta.json();
     const donde = document.querySelectorAll(".tarjeta-md");
     donde.forEach(function (t, i) {
-        donde[i].innerHTML += '<a href="articulo.html?nro='+(i+1)+'" ><div class="tarjeta" style="background : url('+ data[i+1].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i+1].title + '<p>' + data[i+1].text[0] + '</p></h3><h4>'+data[i+1].year +'</h4></div></a>';
+        donde[i].innerHTML += '<a href="articulo.html?nro='+(i+2)+'" ><div class="tarjeta" style="background : url('+ data[i+2].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i+2].title + '<p>' + data[i+2].text[0] + '</p></h3><h4>'+data[i+2].year +'</h4></div></a>';
     });
 }
 async function tarjetalg() {
-    const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolioAnim2023/main/works.json");
+    const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolio2023/main/works.json");
     const data = await consulta.json();
     const donde = document.querySelectorAll(".tarjeta-lg");
     donde.forEach(function (t, i) {
